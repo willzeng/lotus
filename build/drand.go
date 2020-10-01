f2c78faa9cdb5570fbd74e172bde551192031253
Hector Sanjuan <code@hector.link>
2020-10-01 14:35:48 +0200

    Re-instantiate https endpoints

--- build/drand.go
+++ build/drand.go
@@ -32,9 +32,9 @@ const (
 var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 	DrandMainnet: {
 		Servers: []string{
-			"http://api.drand.sh",
-			"http://api2.drand.sh",
-			"http://api3.drand.sh",
+			"https://api.drand.sh",
+			"https://api2.drand.sh",
+			"https://api3.drand.sh",
 			"https://drand.cloudflare.com",
 		},
 		Relays: []string{
